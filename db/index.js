79e44e538527e3ca2fc21a90ead9f86c8f6c95db
Siddhant Sharma <siddhant4040s@gmail.com>
2025-10-10 23:24:29 +0530

    feat: Configure initial MongoDB connection
    Creates a db/ directory and module for database logic. Sets up the index file to initialize the connection on startup.

---
+++ db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {DB_Name} from '../constraints.js';
+const connDb=(async()=>{
+    try{
+      const ConnectionInstance= await mongoose.connect(`${process.env.URI}/${DB_Name}`,)
+     console.log(`Database connected Successfully:${ConnectionInstance.connection.host}`)
+    }
+    catch(err){
+        console.error("Error:",err);
+        process.exit(1);
+
+    }
+})
+export default connDb;
---
+++ index.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import {DB_Name} from './constraints.js';
+import express from "express"
+import connDb from "./db/index.js";
+const app=express();
