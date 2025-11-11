c8ccad73b8801162ba51486f88942884a65e3f0d
Siddhant Sharma <siddhant4040s@gmail.com>
2025-11-11 23:40:09 +0530

    feat(user):user schema for login

---
+++ modules/User.js
@@ -0,0 +1,13 @@
+// models/User.js
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true },
+  id:{type: String, required: true, unique: true, lowercase: true, trim: true},
+  // store active refresh tokens (you can limit size or store hashes in prod)
+  refreshTokens: { type: [String], default: [] },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export const User = mongoose.model("User", UserSchema);
